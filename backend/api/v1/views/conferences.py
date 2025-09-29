aa7a6f21f49069a68cd6cec44f67ecbc3ff42c59
owino600 <owinodaniel600@gmail.com>
2025-09-29 09:57:43 +0300

    api updates

---
+++ backend/api/v1/views/conferences.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""create a new view for objects that
+handles all default RESTFul API actions
+"""
+from flask import Flask, jsonify, request, abort
+from models import storage
+from models. import
+from models. import
+from api.v1.views import app_views
